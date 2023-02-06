1f78c3c7e2ec7882e4af6a308ec9fd1f80d11cd4
Jacob Truong <jacob.truong@ymail.com>
2023-02-06 21:44:23 +0800

    display

--- components/playground/monster-card.js
+++ components/playground/monster-card.js
@@ -4,33 +4,36 @@ import Image from "next/image";
 function DisplayResult(props) {
   const monster = props.monster;
   console.log(monster);
-  return (
-    <div className={classes.monstercard}>
-      <p style={{ fontSize: "35px", fontWeight: "600" }}>{monster.name}</p>
-      <Image
-        src={monster.img}
-        alt="godzilaicon"
-        className={classes.monsterimage}
-        loading="eager"
-        priority={true}
-        width={500}
-        height={500}
-      />
-      <p>
-        <span className={classes.titles}>First Appear</span>:{" "}
-        {monster.firstAppearance}
-      </p>
-      <p>
-        <span className={classes.titles}>Ability</span>: {monster.ability}
-      </p>
-      <a
-        href={monster.link}
-        style={{ margin: "0" }}
-        className={classes.learnmorebutton}
-      >
-        Learn more
-      </a>
-    </div>
-  );
+
+  if (monster) {
+    return (
+      <div className={classes.monstercard}>
+        <p style={{ fontSize: "35px", fontWeight: "600" }}>{monster.name}</p>
+        <Image
+          src={monster.img}
+          alt="godzilaicon"
+          className={classes.monsterimage}
+          loading="eager"
+          priority={true}
+          width={500}
+          height={500}
+        />
+        <p>
+          <span className={classes.titles}>First Appear</span>:{" "}
+          {monster.firstAppearance}
+        </p>
+        <p>
+          <span className={classes.titles}>Ability</span>: {monster.ability}
+        </p>
+        <a
+          href={monster.link}
+          style={{ margin: "0" }}
+          className={classes.learnmorebutton}
+        >
+          Learn more
+        </a>
+      </div>
+    );
+  }
 }
 export default DisplayResult;
